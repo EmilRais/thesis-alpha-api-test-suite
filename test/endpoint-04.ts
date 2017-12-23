16a741d763026fd224113e9c988ede9c1b371da2
Emil Rais <emilrais@outlook.com>
2017-12-23 09:24:54 +0100

    Wrote tests for endpoint 4 and alpha-api-manual passes

---
+++ test/endpoint-04.ts
@@ -0,0 +1,62 @@
+import * as chai from "chai";
+const should = chai.should();
+
+import { Db, MongoClient } from "mongodb";
+import * as agent from "superagent";
+
+describe("Endpoint 4 - POST /post/update", () => {
+    let database: Db;
+
+    before(() => {
+        return MongoClient.connect("mongodb://localhost:27017/database")
+            .then(db => database = db);
+    });
+
+    afterEach(() => {
+       return database.dropDatabase();
+    });
+
+    after(() => {
+        return database.close();
+    });
+
+    it("1. Lykkes det ikke at opdatere opslaget skal endpointet returnere Internal Server Error.", () => {
+        const post = { _id: "some-id", title: "some-title"};
+        return agent.post("localhost:3030/post/update")
+            .send(post)
+            .catch(error => error.response)
+            .then(response => {
+                response.status.should.equal(500);
+            });
+    });
+
+    it("2. Lykkes det at opdatere opslaget skal endpointet returnere statussen OK.", () => {
+        const oldPost = { _id: "some-id", title: "old-title" };
+        const newPost = { _id: "some-id", title: "some-title"};
+        return database.collection("Posts").insert(oldPost)
+            .then(() => {
+                return agent.post("localhost:3030/post/update")
+                    .send(newPost)
+                    .catch(error => error.response)
+                    .then(response => {
+                        response.status.should.equal(200);
+                    });
+            });
+    });
+
+    it("3. Lykkes det at opdatere opslaget forefindes det opdaterede opslag i databasen.", () => {
+        const oldPost = { _id: "some-id", title: "old-title" };
+        const newPost = { _id: "some-id", title: "some-title"};
+        return database.collection("Posts").insert(oldPost)
+            .then(() => {
+                return agent.post("localhost:3030/post/update")
+                    .send(newPost)
+                    .catch(error => error.response)
+                    .then(response => {
+                        return database.collection("Posts").find({}, { title: true }).toArray()
+                            .then(posts => posts.should.deep.equal([newPost]));
+                    });
+            });
+    });
+
+});
